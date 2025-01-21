1ec1d9e1431bdc985991263bf92994adba4cd5b3
chinthalamanikanta <143411132+chinthalamanikanta@users.noreply.github.com>
2025-01-21 11:33:30 +0530

    Update TimesheetController.java

--- src/main/java/com/project/middleware/TimesheetModule/controller/TimesheetController.java
+++ src/main/java/com/project/middleware/TimesheetModule/controller/TimesheetController.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "manibackendts-f7fee3fvgxdchebf.canadacentral-01.azurewebsites.net")
 @RequestMapping("/api/timesheets")
 public class TimesheetController {
 
